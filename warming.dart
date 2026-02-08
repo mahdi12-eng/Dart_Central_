9a3b0be6a3ff387e7a26e0165bffb560914d2e61
Mahdi <hatamimahdi32@gmail.com>
2026-02-08 21:12:05 +0430

    add two numbers too warmly

---
+++ warming.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  // use a variable to store the returned value
+  int total = sumOfNumber(3, 3452);
+
+  // print the returned value
+  print('the sum of numbers are $total ');
+}
+
+// create a function of type integer for adding two numbers together
+int sumOfNumber(int number1, int number2){
+
+  // return the summation of the numbers
+  return number1 + number2;
+
+}
