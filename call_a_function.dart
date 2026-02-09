bd0cd2f2c71d19ea56159983d273aa60d3c876ef
Mahdi <hatamimahdi32@gmail.com>
2026-02-09 15:58:27 +0430

    call a function in various way

---
+++ call_a_function.dart
@@ -0,0 +1,14 @@
+void main(){
+
+
+  addNumber(12, 34);
+}
+addNumber(int a , int b){
+  print(a+b);
+  printFullName(name: 'mahdi',lastName: 'hatami');
+
+}
+printFullName({String? name, String?lastName}){
+  print(name! + lastName!);
+
+}
