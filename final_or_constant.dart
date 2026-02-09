7d31566533b0422e01fe2791ee42189a3093bdf1
Mahdi <hatamimahdi32@gmail.com>
2026-02-09 15:59:17 +0430

    the diference between constan ad final

---
+++ final_or_constant.dart
@@ -0,0 +1,20 @@
+void main(){
+
+
+  // show an incoming message
+  print("Hi dears how is it going?");
+
+  // store the user name
+  String username = "Mahdi Hatami";
+
+  //print the user name
+  print(username);
+  // get a final variable
+  final number = username.length;
+
+  // compute the username length as a constant
+  const total_number = 12;
+
+  // display the final result of the constant VS final
+print("This is the final number\" $number\" and this one is constant number named total_constant \"$total_number\"");
+}
